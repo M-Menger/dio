8740112b2bc9d6f0b4fc795e4b592bdab19144f8
Matheus Menger <matheus-menger@hotmail.com>
2024-09-13 19:10:04 -0300

    Tipos Primitivos

---
+++ aula-variaveis-dio/src/TiposPrimitivos.java
@@ -0,0 +1,18 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) throws Exception {
+        // Tipos Primitivos Númericos:
+        byte num1 = 1;
+        short num2 = 1;
+        int num3 = 1;
+        long num4 = 1L;
+        float num5 = 1.0F;
+        double num6 = 1.0;
+
+        System.out.println("Byte entre -127 e 127" + num1);
+        System.out.println("Short entre ");
+        System.out.println("Int entre ");
+        System.out.println("Long entre ");
+        System.out.println("Float entre ");
+        System.out.println("Double entre ");   
+    }
+}
